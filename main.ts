0d79bbd1ace5b8b933d03f4e03490ad4ff785d8a
Quintin Donnelly <quinndonnelly22@gmail.com>
2024-03-03 14:45:45 -0500

    chore: cleaning up vistigial code (#7)
    * chore: cleaning up vistigial code
    * feat: updating README with instructions

--- main.ts
+++ main.ts
@@ -1,75 +1,19 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Editor, MarkdownView, Plugin } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-    mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-    mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-    settings: MyPluginSettings;
+export default class ObsidianClipboard extends Plugin {
 
     async onload() {
-        await this.loadSettings();
-
         // This adds an editor command that can perform some operation on the current editor instance
         this.addCommand({
             id: 'copy-with-format',
             name: 'Copy With Format',
-            editorCallback: (editor: Editor, view: MarkdownView) => {
+            editorCallback: (editor: Editor, _: MarkdownView) => {
                 const selectedText = editor.getSelection();
                 const filteredText = removeObsidianLinks(selectedText);
                 // will call to process the text here
                 navigator.clipboard.writeText(filteredText);
             }
         });
-        // This adds a complex command that can check whether the current state of the app allows execution of the command
-        this.addCommand({
-            id: 'open-sample-modal-complex',
-            name: 'Open sample modal (complex)',
-            checkCallback: (checking: boolean) => {
-                // Conditions to check
-                const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-                if (markdownView) {
-                    // If checking is true, we're simply "checking" if the command can be run.
-                    // If checking is false, then we want to actually perform the operation.
-                    if (!checking) {
-                        new SampleModal(this.app).open();
-                    }
-
-                    // This command will only show up in Command Palette when the check function returns true
-                    return true;
-                }
-            }
-        });
-
-        // This adds a settings tab so the user can configure various aspects of the plugin
-        this.addSettingTab(new SampleSettingTab(this.app, this));
-
-        // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-        // Using this function will automatically remove the event listener when this plugin is disabled.
-        this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-            console.log('click', evt);
-        });
-
-        // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-        this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-    }
-
-    onunload() {
-
-    }
-
-    async loadSettings() {
-        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-    }
-
-    async saveSettings() {
-        await this.saveData(this.settings);
     }
 }
 
@@ -77,47 +21,5 @@ function removeObsidianLinks(text: string): string {
     // finds all occurrences of either [[ or ]]
     const expression = "(?:\\[\\[)|(?:\\]\\])";
     const mode = "g";
-    return text.replace(new RegExp(expression, mode), "").replace(new RegExp(expression, mode), "");
-}
-
-class SampleModal extends Modal {
-    constructor(app: App) {
-        super(app);
-    }
-
-    onOpen() {
-        const { contentEl } = this;
-        contentEl.setText('Woah!');
-    }
-
-    onClose() {
-        const { contentEl } = this;
-        contentEl.empty();
-    }
-}
-
-class SampleSettingTab extends PluginSettingTab {
-    plugin: MyPlugin;
-
-    constructor(app: App, plugin: MyPlugin) {
-        super(app, plugin);
-        this.plugin = plugin;
-    }
-
-    display(): void {
-        const { containerEl } = this;
-
-        containerEl.empty();
-
-        new Setting(containerEl)
-            .setName('Setting #1')
-            .setDesc('It\'s a secret')
-            .addText(text => text
-                .setPlaceholder('Enter your secret')
-                .setValue(this.plugin.settings.mySetting)
-                .onChange(async (value) => {
-                    this.plugin.settings.mySetting = value;
-                    await this.plugin.saveSettings();
-                }));
-    }
+    return text.replace(new RegExp(expression, mode), "");
 }
