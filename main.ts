5f6ea9eacd5c3cdaff0c1e8d10a94dd264530b1a
Quinn Donnelly <quinndonnelly22@gmail.com>
2024-03-03 11:39:49 -0500

    feat: grab text from selection and put in clipboard
    closes #3 #4

--- main.ts
+++ main.ts
@@ -16,33 +16,14 @@ export default class MyPlugin extends Plugin {
     async onload() {
         await this.loadSettings();
 
-        // This creates an icon in the left ribbon.
-        const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-            // Called when the user clicks the icon.
-            new Notice('This is a notice!');
-        });
-        // Perform additional things with the ribbon
-        ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-        // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-        const statusBarItemEl = this.addStatusBarItem();
-        statusBarItemEl.setText('Status Bar Text');
-
-        // This adds a simple command that can be triggered anywhere
+       // This adds an editor command that can perform some operation on the current editor instance
         this.addCommand({
-            id: 'open-sample-modal-simple',
-            name: 'Open sample modal (simple)',
-            callback: () => {
-                new SampleModal(this.app).open();
-            }
-        });
-        // This adds an editor command that can perform some operation on the current editor instance
-        this.addCommand({
-            id: 'sample-editor-command',
-            name: 'Sample editor command',
+            id: 'copy-with-format',
+            name: 'Copy With Format',
             editorCallback: (editor: Editor, view: MarkdownView) => {
-                console.log(editor.getSelection());
-                editor.replaceSelection('Sample Editor Command');
+                const selectedText = editor.getSelection();
+                // will call to process the text here
+                navigator.clipboard.writeText(selectedText);
             }
         });
         // This adds a complex command that can check whether the current state of the app allows execution of the command
